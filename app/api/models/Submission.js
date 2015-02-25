f3e516babcc42e4c6727fae21776a4b783956836
Tamara Chahine <chahine.tamara@abc.net.au>
2015-02-25 14:51:50 +1100

    Added submission model and controller

---
+++ app/api/models/Submission.js
@@ -0,0 +1,52 @@
+/**
+ * Submission
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+  types: {
+    // location: function(location) {
+    //   location = JSON.parse(location);
+    //   return (location.latitude && location.longitude);
+    // }
+  },
+  attributes: {
+    stop_id: {
+      type: 'string',
+      required: true
+    },
+    user_fingerprint: {
+      type: 'text',
+      required: true
+    },
+    latitude: {
+      type: 'float',
+      required: true
+    },
+    longitude: {
+      type: 'float',
+      required: true
+    },
+    route_id: {
+      type: 'string',
+      required: true
+    },
+    late: {
+      type: 'boolean',
+      required: true
+    },
+    capacity: {
+      type: 'integer',
+      required: true
+    }
+  },
+
+  beforeCreate: function(values, cb){
+    console.log(values);
+    cb();
+  }
+};
+
